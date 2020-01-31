9d73174d1ac1661ea7d3ae3607e88d24ce158f3c
Miguel Yann <tsambamiguelito@gmail.com>
2020-01-31 20:52:10 +0100

    Updates

---
+++ precache-manifest.c0582c372f8f77200530f0553b4e4e08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6975690db20ce5639df9291e627a4c5",
+    "url": "/FakeTrainLine/index.html"
+  },
+  {
+    "revision": "5f3b53f5eb174f343436",
+    "url": "/FakeTrainLine/static/css/main.cb6baafc.chunk.css"
+  },
+  {
+    "revision": "0f3e27ff6315593c5e8b",
+    "url": "/FakeTrainLine/static/js/2.bff28034.chunk.js"
+  },
+  {
+    "revision": "13425e2e45fee351b4139959073d7e4f",
+    "url": "/FakeTrainLine/static/js/2.bff28034.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f3b53f5eb174f343436",
+    "url": "/FakeTrainLine/static/js/main.49f42002.chunk.js"
+  },
+  {
+    "revision": "d999e9db0c46b7fc6cb6",
+    "url": "/FakeTrainLine/static/js/runtime-main.b81c2f4d.js"
+  }
+]);
